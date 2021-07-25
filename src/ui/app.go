d31f728dff159869bb997a85b479f744b3f5b384
Paweł Kamiński <pawel13k@gmail.com>
2021-07-25 22:29:52 +0200

    feat: Possibility to add a new task to todo

--- src/ui/app.go
+++ src/ui/app.go
@@ -13,6 +13,10 @@ type TaskBox struct {
 	List *tasks.List
 }
 
+var app *tview.Application
+var pages *tview.Pages
+var inputField *tview.InputField
+
 func textObject(text string) tview.Primitive {
 	return tview.NewTextView().
 		SetTextAlign(tview.AlignLeft).
@@ -50,24 +54,26 @@ func (task *TaskBox) Draw(screen tcell.Screen) {
 		SetFixed(1, 2).
 		SetDoneFunc(func(key tcell.Key) {
 			if key == tcell.KeyEscape {
-				// TODO: Close the application
-				//app.Stop()
+				app.Stop()
 			}
 			if key == tcell.KeyEnter {
-				// TODO Open view of a sinlge task
 				task.Table.SetSelectable(true, true)
 			}
 		}).
 		SetSelectedFunc(func(row int, column int) {
 			task.List.ToggleDone(row)
-			// TODO: open task details
+            // Moze selected task to a next one
+            // TODO: Make sure we do not overflow cells
+            task.Table.Select(row + 1, column)
 		}).
 		SetInputCapture(func(capture *tcell.EventKey) *tcell.EventKey {
-			if capture.Rune() == ' ' {
-				// TODO: mark task as done
+            // Open adding new task when pressing "c"
+            if capture.Rune() == 'c' {
+                pages.ShowPage("modal")
+                app.SetFocus(inputField)
 
 				return nil
-			}
+		    }
 
 			return capture
 		})
@@ -75,8 +81,11 @@ func (task *TaskBox) Draw(screen tcell.Screen) {
 	task.Table.Draw(screen)
 }
 
+
 func Main() {
-	app := tview.NewApplication()
+	app = tview.NewApplication()
+
+    pages = tview.NewPages()
 
     storage := storage.NewDisk("storage.json")
     list, err := storage.LoadTasks()
@@ -96,12 +105,60 @@ func Main() {
 	tbox.
 		SetBackgroundColor(tcell.NewHexColor(0x2E3440))
 
-	flex := tview.NewFlex().
+	modal := func(p tview.Primitive, width, height int) tview.Primitive {
+
+        frame := tview.NewFrame(p).
+            SetBorders(0, 1, 0, 0, 1, 1).
+            AddText(" === New task === ", true, 1, tcell.NewHexColor(0xD8DEE9))
+
+        frame.SetBorder(false).
+            SetBackgroundColor(tcell.NewHexColor(0x3B4252))
+
+        return tview.NewFlex().
+			AddItem(nil, 0, 1, false).
+			AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+				AddItem(nil, 0, 1, false).
+                AddItem(frame, height + 2, 1, false).
+				AddItem(nil, 0, 1, false), width + 2, 1, false).
+			AddItem(nil, 0, 1, false)
+	}
+
+    inputField = tview.NewInputField().
+	  SetFieldTextColor(tcell.NewHexColor(0xD8DEE9)).
+      SetFieldBackgroundColor(tcell.NewHexColor(0x4C566A))
+
+    donefunc := func(key tcell.Key) {
+        text := inputField.GetText()
+
+        if key == tcell.KeyEnter && text != "" {
+            list.Add(text)
+        }
+
+        inputField.SetText("")
+        pages.HidePage("modal")
+        //pages.SwitchToPage("background")
+        app.SetFocus(&tbox)
+    }
+
+    inputField.SetDoneFunc(func(key tcell.Key) {
+		    //app.Stop()
+            donefunc(key)
+	})
+
+    flex := tview.NewFlex().
 		SetFullScreen(true).
 		SetDirection(tview.FlexRow).
 		AddItem(&tbox, 0, 1, false)
 
-	if err := app.SetRoot(flex, true).SetFocus(&tbox).Run(); err != nil {
+    newModal := modal(inputField, 40, 1)
+
+	pages.
+		AddPage("background", flex, true, true).
+		AddPage("modal", newModal, true, true)
+
+    pages.HidePage("modal")
+
+	if err := app.SetRoot(pages, true).SetFocus(&tbox).Run(); err != nil {
 		panic(err)
 	}
 }
